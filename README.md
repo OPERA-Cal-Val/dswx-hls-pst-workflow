4e3961efbff19d3ab3d26c121e627f2de3039a8e
Charlie Marshak <charlie.z.marshak@jpl.nasa.gov>
2023-12-20 18:00:09 -0800

    update for jj

--- README.md
+++ README.md
@@ -5,12 +5,18 @@ See: https://github.com/nasa/PROTEUS
 Basically, use the notebook to take an HLS_ID and output a DSWx Product.
 
 
-## Installation
+## Installation and Setup.
 
-1. Clone this repository and navigate to it.
-2. `mamba env update -f environment.yml`
-3. `conda activate dswx_hls`
-4. `python -m ipykernel install --user --name dswx_hls`
-5. `jupyter-lab` and open the Workflow
+Clone this repository and navigate to it. Create an `.env` file in this directory with your earthdata username and password as:
+
+```
+ED_USERNAME=<USERNAME>
+ED_PASSWORD=<PASSWORD>
+```
+
+1. `mamba env update -f environment.yml`
+2. `conda activate dswx_hls_generation`
+3. `python -m ipykernel install --user --name dswx_hls_generation`
+4. `jupyter-lab` and open the workflow notebook
 
 This only works specifying a single HLS IDs. All the necessary datasets are localized.
---
+++ run_validation.py
@@ -0,0 +1,21 @@
+import geopandas as gpd
+import papermill as pm
+from tqdm import tqdm
+from pathlib import Path
+
+df_val = gpd.read_file('https://raw.githubusercontent.com/OPERA-Cal-Val/DSWx-Requirement-Verification/dev/dswx_verification/data/validation_table.geojson')
+hls_ids = df_val.hls_id
+
+TEMPLATE_NB = '0-Run-DSWX-HLS.ipynb'
+NB_OUT_DIR = Path('out_notebooks')
+NB_OUT_DIR.mkdir(exist_ok=True, parents=True)
+
+for aerosol_param in [True, False]:
+    for hls_id in tqdm(hls_ids):
+        aero_token = 'on' if aerosol_param else 'off'
+        pm.execute_notebook(TEMPLATE_NB,
+                            NB_OUT_DIR / f'{hls_id}-aerosol_{aero_token}.ipynb',
+                            parameters=dict(HLS_ID_OR_HLS_DIR_PATH=hls_id,
+                                            OUT_DIR=f'out-aerosol_{aero_token}',
+                                            DOWNLOAD_HLS=True,
+                                            AEROSOL_PARAM=aerosol_param))
